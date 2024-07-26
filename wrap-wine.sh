2b4719d956d6e0b65b3a161a12a2f39a98156fdf
limliu <lim.liu.01@outlook.com>
2024-07-26 16:05:28 +0800

    add winepath command

--- wrap-wine.sh
+++ wrap-wine.sh
@@ -14,6 +14,10 @@ cat << EOF > /usr/local/bin/wineserver
 #!/bin/sh
 WINEPREFIX=~/.wine64 WINEARCH=win64 box64 ~/wine/bin/wineserver \$@
 EOF
+cat << EOF > /usr/local/bin/winepath
+#!/bin/sh
+WINEPREFIX=~/.wine64 WINEARCH=win64 box64 ~/wine/bin/winepath \$@
+EOF
 ln -s ~/wine/bin/wineboot /usr/local/bin/wineboot
 ln -s ~/wine/bin/winecfg /usr/local/bin/winecfg
-chmod +x /usr/local/bin/wine /usr/local/bin/wine64 /usr/local/bin/wineboot /usr/local/bin/winecfg /usr/local/bin/wineserver
+chmod +x /usr/local/bin/wine /usr/local/bin/wine64 /usr/local/bin/wineboot /usr/local/bin/winecfg /usr/local/bin/wineserver /usr/local/bin/winepath
